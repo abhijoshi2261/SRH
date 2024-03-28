bed6eaff224675f1fc1287cbfd6c32b7ca9fdba6
abhijoshi2261 <joshhiabhishek@gmail.com>
2024-03-28 23:33:43 +0530

    initial commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+[x: string]: any;
+
+  phoneNumber="+222-1800-2628";
+  email='blueskytechcompany@gmail.com';
+
+}
